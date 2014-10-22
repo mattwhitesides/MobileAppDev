5f740904cab90bc54cc46aef6b4481993769f676
Matt Whitesides <mbwxd4@mail.missouri.edu>
2014-10-22 10:00:39 -0500

    Tutorial Part 2 Done

--- Lectures/Tutorial1/Tutorial1/ViewController.swift
+++ Lectures/Tutorial1/Tutorial1/ViewController.swift
@@ -10,22 +10,70 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    @IBOutlet var appsTableView : UITableView?
+    
+    var tableData = []
+    
+    func searchItunesFor(searchTerm : String) {
+        let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: .CaseInsensitiveSearch, range: nil)
+        
+        if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
+            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+            let url = NSURL(string: urlPath)
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
+                println("Task completed")
+                if(error != nil) {
+                    // If there is an error in the web request, print it to the console
+                    println(error.localizedDescription)
+                }
+                var err: NSError?
+                
+                var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+                if(err != nil) {
+                    // If there is an error parsing JSON, print it to the console
+                    println("JSON Error \(err!.localizedDescription)")
+                }
+                let results: NSArray = jsonResult["results"] as NSArray
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.tableData = results
+                    self.appsTableView!.reloadData()
+                })
+            })
+            task.resume()
+        }
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return tableData.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell:UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        let rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
         
-        cell.textLabel?.text = "Row #\(indexPath.row)"
-        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        cell.textLabel?.text = rowData["trackName"] as? String
+        
+        //Get Artwork
+        let urlString: NSString = rowData["artworkUrl60"] as NSString
+        let imgURL: NSURL = NSURL(string: urlString)
+        
+        //Get Image Data
+        let imgData: NSData = NSData(contentsOfURL: imgURL)
+        cell.imageView?.image = UIImage(data: imgData)
+        
+        //Get formatted string to subtitle data
+        let formattedPrice: NSString = rowData["formattedPrice"] as NSString
+
+        cell.detailTextLabel?.text = formattedPrice
         
         return cell
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        searchItunesFor("EA")
     }
 
     override func didReceiveMemoryWarning() {
